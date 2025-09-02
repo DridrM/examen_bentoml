3bcef5d5d81c27639ebb1e0b526de1894ac0cb7f
DridrM <DridrM@proton.me>
2025-09-02 20:52:26 +0200

    Build bento with pyproject.toml

--- src/examen_bentoml/prepare_data.py
+++ src/examen_bentoml/prepare_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 
 from examen_bentoml.params import RAW_DATA_PATH, PROCESSED_DATA_PATH
 
